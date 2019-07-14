5a086fa5b6c38fd0a773e4b1e148b40642937e0c
eldaPro <faith030@gmail.com>
2019-07-14 23:12:14 +0700

    create component - Register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // ---------------------------------------------- modules import
 import React from "react";
 
-import Login from "./components/auth/login";
+// import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 // import Dashboard from "./components/layout/dashboard";
 import Navbar from "./components/layout/navbar";
 
@@ -9,7 +10,7 @@ import Navbar from "./components/layout/navbar";
 const App = () => (
   <div className="App">
     <Navbar />
-    <Login />
+    <Register />
   </div>
 );
 
